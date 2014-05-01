3fba7b17b55162c5fd8ac3fc1675804fac8e0dc8
Sara <sara@sarabine.com>
2014-05-01 06:38:17 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,202 @@
+<?php include('header.php'); ?>
+<div id="content">
+	<div id="synopsis">
+		<p>I'm a Denver-based Web Developer with a <a class="cursive" href="#about">passion</a> for cutting-edge technology.</p>
+		<p>Like what you see? Email me at  <a class="email" title="Email me"></a></p>
+	</div>
+
+	<div class="page" id="work">
+		<h2>My Work</h2>
+		<section>
+			<article class="project">
+				<a class="thumbnail" href="http://transactionalfunding.com" title="Transactional Funding" target="_blank"><img src="portfolio/tf.png" alt="Transactional Funding" /></a>
+				<div class="description">
+					<a href="http://transactionalfunding.com" title="Transactional Funding" target="_blank"><h3>Transactional Funding</h3></a>
+					<p>I integrated a client's <a href="http://wordpress.org/" title="WordPress" target="_blank">WordPress</a> site with the <a href="http://recurly.com/" title="Recurly" target="_blank">Recurly</a> API via a custom plugin.</p>
+				</div>
+			</article>
+			<article class="project">
+				<a class="thumbnail" href="http://itunes.apple.com/us/app/bop-it-!-smash/id523662874?mt=8" title="Bop It! Smash" target="_blank"><img src="portfolio/bopitsmash.png" alt="Bop It! Smash" /></a>
+				<div class="description">
+					<a href="http://itunes.apple.com/us/app/bop-it-!-smash/id523662874?mt=8" title="Bop It! Smash" target="_blank"><h3>Bop It! Smash</h3></a>
+					<p>I managed the data interchange behind iOS app Bop It! Smash with JSON, including user segmentation and in-app promotions.</p>
+				</div>
+			</article>
+			<article class="project">
+				<a class="thumbnail" href="consciouscreations" title="Conscious Creations Jewelry" target="_blank"><img src="portfolio/cc.png" alt="Conscious Creations Jewelry" /></a>
+				<div class="description">
+					<a href="consciouscreations" title="Conscious Creations Jewelry" target="_blank"><h3>Conscious Creations Jewelry</h3></a>
+					<p>I customized a website template for a freelance client.</p>
+				</div>
+			</article>
+			<article class="project">
+				<a class="thumbnail" href="http://thedylanjones.com" title="Dylan Jones" target="_blank"><img src="portfolio/thedylanjones.png" alt="Dylan Jones" /></a>
+				<div class="description">
+					<a href="http://thedylanjones.com" title="Dylan Jones" target="_blank"><h3>Dylan Jones</h3></a>
+					<p>I coded a personal portfolio website for a freelance client.</p>
+				</div>
+			</article>
+			<article class="project">
+				<a class="thumbnail" href="http://itunes.apple.com/us/app/ultimate-battle-zombies/id480306077?mt=8" title="Ultimate Battle Zombies" target="_blank"><img src="portfolio/ubz.png" alt="Ultimate Battle Zombies" /></a>
+				<div class="description">
+					<a href="http://itunes.apple.com/us/app/ultimate-battle-zombies/id480306077?mt=8" title="Ultimate Battle Zombies" target="_blank"><h3>Ultimate Battle Zombies</h3></a>
+					<p>I was responsible for all data interchange behind iOS game Ultimate Battle Zombies and developed several mobile websites used in-app.</p>
+				</div>
+			</article>
+			<article class="project">
+				<a class="thumbnail" href="http://itunes.apple.com/us/app/my-pet-zombie/id424615076?mt=8" title="My Pet Zombie" target="_blank"><img src="portfolio/mpz.png" alt="My Pet Zombie" /></a>
+				<div class="description">
+					<a href="http://itunes.apple.com/us/app/my-pet-zombie/id424615076?mt=8" title="My Pet Zombie" target="_blank"><h3>My Pet Zombie</h3></a>
+					<p>I worked on the backend for the My Pet Zombie in-app purchase store and was responsible for all data interchange with JSON.</p>
+				</div>
+			</article>
+			<article class="project">
+				<a class="thumbnail" href="http://profilepicmaker.com" title="Profile Pic Maker" target="_blank"><img src="portfolio/ppm.png" alt="Profile Pic Maker" /></a>
+				<div class="description">
+					<a href="http://profilepicmaker.com" title="Profile Pic Maker" target="_blank"><h3>Profile Pic Maker</h3></a>
+					<p>A profile picture uploader, editor and cropper capitalizing on Facebook's new layout. Written in PHP, JavaScript/jQuery, Ajax, and the PHP library <a href="http://www.imagemagick.org/script/index.php" target="_blank">ImageMagick</a>.</p>
+				</div>
+			</article>
+			<article class="project">
+				<a class="thumbnail" href="portfolio/BombBreak/BombBreak.html" title="Bomb Break" target="_blank"><img src="portfolio/BombBreak/BombBreak.png" alt="Bomb Break" /></a>
+				<div class="description">
+					<a href="portfolio/BombBreak/BombBreak.html" title="Bomb Break" target="_blank"><h3>Bomb Break</h3></a>
+					<p>A experiment using the <a href="http://en.wikipedia.org/wiki/Stroop_effect" target="_blank">Stroop Effect</a> as a game mechanic, featuring an in-depth story line alongside fast-paced gameplay. Written in <a href="http://www.actionscript.org/" target="_blank">ActionScript 3.0</a>.</p>
+				</div>
+			</article>
+			<article class="project">
+				<a class="thumbnail" href="portfolio/twitter/" title="Twitter Aggregator" target="_blank"><img src="portfolio/twitter.png" alt="Twitter Aggregator" /></a>
+				<div class="description">
+					<a href="portfolio/twitter/" title="Twitter Aggregator" target="_blank"><h3>Twitter Aggregator</h3></a>
+					<p>A Twitter feed aggregator that measures and visualizes word frequency in real-time tweets. Written in PHP and <a href="http://www.processing.org/" target="_blank">Processing</a>.</p>
+				</div>
+			</article>
+			<article class="project">
+				<a class="thumbnail" href="http://gresss.com" title="GRESSS" target="_blank"><img src="portfolio/gresss.png" alt="GRESSS" /></a>
+				<div class="description">
+					<a href="http://gresss.com" title="GRESSS" target="_blank"><h3>GRESSS Website</h3></a>
+					<p>I designed and coded a simple website for a freelance client.</p>
+				</div>
+			</article>
+			<article class="project">
+				<a class="thumbnail" href="portfolio/Cellpocalypse.html" title="Cellpocalypse" target="_blank"><img src="portfolio/Cellpocalypse.png" alt="Cellpocalypse" /></a>
+				<div class="description">
+					<a href="portfolio/Cellpocalypse.html" title="Cellpocalypse" target="_blank"><h3>Cellpocalypse</h3></a>
+					<p>A weekend prototype exploring the needs of cooperative play: two players must work together to protect a cell from incoming viruses. Written in <a href="http://www.actionscript.org/" target="_blank">ActionScript 3.0</a>.</p>
+				</div>
+			</article>
+			<article class="project">
+				<a class="thumbnail" href="portfolio/MiniMania.html" title="Mini Mania" target="_blank"><img src="portfolio/MiniMania.png" alt="Mini Mania" /></a>
+				<div class="description">
+					<a href="portfolio/MiniMania.html" title="Mini Mania" target="_blank"><h3>Mini Mania</h3></a>
+					<p>A final project illustrating the difference between a game and a 'toy'. Randomly generated tracks provide the player with an infinite number of new obstacles! Uses 2D game physics and a <a href="http://www.coreyoneil.com/portfolio/index.php?project=5">pixel-perfect collision detection library</a>.</p>
+				</div>
+			</article>
+			<article class="project">
+				<a class="thumbnail" href="portfolio/BigMack" title="Big Mack Attack" target="_blank"><img src="portfolio/BigMack.png" alt="Big Mack Attack" /></a>
+				<div class="description">
+					<a href="portfolio/BigMack" title="Big Mack Attack" target="_blank"><h3>Big Mack Attack</h3></a>
+					<p>An exercise in raising awareness of a social issue through a game. One player navigates through each floor of a fast-food company's headquarters in order to save the city from obesity.</p>
+				</div>
+			</article>
+			<article class="project">
+				<a class="thumbnail" href="portfolio/greeps.html" title="Greeps" target="_blank"><img src="portfolio/Greeps.png" alt="Greeps" /></a>
+				<div class="description">
+					<a href="portfolio/greeps.html" title="Greeps" target="_blank"><h3>Greeps Contest Entry - 2nd Place Winner</h3></a>
+					<p>My submission to DU's <a href="http://www.greenfoot.org/competition/greeps/index.html">Greenfoot Greeps Contest</a>. The goal was to collect the most tomatoes by modifying ONLY the greeps' behavior (no extra memory allocation permitted).</p>
+				</div>
+			</article>
+			<article class="project">
+				<a class="thumbnail" href="portfolio/SpaceFighter/index.html" title="Space Fighter" target="_blank"><img src="portfolio/SpaceFighter.png" alt="Space Fighter" /></a>
+				<div class="description">
+					<a href="portfolio/SpaceFighter/index.html" title="Space Fighter" target="_blank"><h3>Space Fighter</h3></a>
+					<p>My first Java project, originally created in <a href="http://www.greenfoot.org/">Greenfoot</a> and rewritten in <a href="http://www.processing.org">Processing</a>. Maneuver through increasingly cluttered, randomly-generated asteroids to get a highscore.</p>
+				</div>
+			</article>
+		</section>
+	</div>
+
+	<div class="page" id="skills">
+		<h2>Skills</h2>
+		<ul id="skill-list" class="inline-list styled-links">
+			<li>HTML</li>
+			<li>CSS</li>
+			<li>JavaScript</li>
+			<li>jQuery</li>
+			<li>PHP</li>
+			<li>Laravel</li>
+			<li>CodeIgniter</li>
+			<li>Magento</li>
+			<li>Bootstrap</li>
+			<li>Responsive Design</li>
+			<li>MySQL</li>
+			<li>MongoDB</li>
+			<li>API Development</li>
+			<li>Git</li>
+			<li>SVN</li>
+			<li>Server Administration</li>
+		</ul>
+	</div>
+	<div class="page" id="about">
+		<h2>About Me</h2>
+		<section>
+			<div class="thumbnail"><img id="me" src="assets/img/me.jpg" alt="me" /></div>
+			<div class="description">
+				<p>I'm a graduate from the University of Denver with a Bachelor's degree in Computer Science and a focus in Digital Media. I have a strong background in web development and game programming, and particularly enjoy learning and leveraging the latest web technologies.</p>
+				<p>I discovered my love for computers through Internet Relay Chat, which led me to my <a href="http://www.ikick.eu" title="iKick.eu" target="_blank">first coding project</a> spanning 4 years and containing over 10,000 lines of code.</p>
+				<p>Currently, I'm employed at an innovative e-commerce company and maintain several side jobs including website consulting and development.</p>
+			</div>
+		</section>
+	</div>
+
+	<!--
+		Hey! I see you found this page I scrapped. Enjoy!
+	-->
+	<!--
+	<div class="page" id="education">
+		<h2>Education</h2>
+		<h3>Computer Science</h3>
+		<ul>
+			<li><span class="class-name">Java I</span></li>
+			<li><span class="class-name">Java II</span></li>
+			<li><span class="class-name">Java III</span></li>
+			<li><span class="class-name">Discrete Structures</span> &nbsp;// Computer Science-related math</li>
+			<li><span class="class-name">Data Structures and Algorithms</span> &nbsp;// Algorithm optimization</li>
+			<li><span class="class-name">Computer Organization</span> &nbsp;// Assembly analysis</li>
+			<li><span class="class-name">Computing and Society</span> &nbsp;// Effects of technology on society</li>
+			<li><span class="class-name">Systems Programming</span> &nbsp;// C and C++</li>
+			<li><span class="class-name">Operating Systems</span> &nbsp;// C, <a href="http://www.pintool.org/" title="pin tool" target="new">pin tool</a>, <a href="http://www.gnuplot.info/" title="gnuplot" target="new">gnuplot</a></li>
+			<li><span class="class-name">Software Tools</span> &nbsp;// shell scripting: bash, awk, sed, <a href="http://www.perl.org/" title="perl" target="new">perl</a>, <a href="http://www.python.org/" title="python" target="new">python</a>, <a href="http://php.net/" title="PHP" target="new">PHP</a>, <a href="http://www.latex-project.org/" title="LaTeX" target="new">LaTeX</a></li>
+			<li><span class="class-name">Introduction to iPhone Programming</span> &nbsp;// Objective C, <a href="http://www.gnustep.org/" title="GNUstep" target="new">GNUstep</a></li>
+			<li><span class="class-name">Introduction to Game Design</span> &nbsp;// <a href="http://www.actionscript.org/" title="ActionScript 3.0" target="new">ActionScript 3.0</a></li>
+			<li><span class="class-name">Database Organization &amp; Management</span> &nbsp;// Database optimization, <a href="http://www.oracle.com/us/products/database/index.html" title="Oracle" target="new">Oracle</a></li>
+			<li><span class="class-name">iPhone Game Development</span> &nbsp;// Objective C, <a href="http://www.openframeworks.cc/" title="openFrameworks" target="new">openFrameworks</a></li>
+			<li><span class="class-name">Game Programming I</span> &nbsp;// C#, <a href="http://msdn.microsoft.com/en-us/library/bb200104.aspx" title="XNA" target="new">XNA</a></li>
+			<li><span class="class-name">Software Engineering</span> &nbsp;// Software development theory</li>
+			<li><span class="class-name">Secure Software</span> &nbsp;// Security vulnerability awareness and prevention</li>
+			<li><span class="class-name">Computer Forensics</span> &nbsp;// Forensical analysis, methods, and tools</li>
+		</ul>
+		<h3>Digital Media Studies</h3>
+		<ul>
+			<li><span class="class-name">Website Design and Management</span> &nbsp;// Basic web design</li>
+			<li><span class="class-name">Technical Foundations in Digital Media</span> &nbsp;// <a href="http://processing.org/" title="Processing" target="new">Processing</a></li>
+			<li><span class="class-name">Programming for Play</span> &nbsp;// <a href="http://www.actionscript.org/" title="ActionScript 3.0" target="new">ActionScript 3.0</a></li>
+			<li><span class="class-name">Web 2.0 Design &amp; Content Management</span> &nbsp;// User Experience Design</li>
+			<li><span class="class-name">Introduction to 3D Modeling</span> &nbsp;// <a href="http://usa.autodesk.com/adsk/servlet/pc/index?siteID=123112&amp;id=13577897" title="Autodesk Maya" target="new">Autodesk Maya</a></li>
+			<li><span class="class-name">Introduction to Electronic Media Art and Design</span> &nbsp;// New Media Art</li>
+		</ul>
+	</div>
+	-->
+
+	<div class="page" id="contact">
+		<h2>Contact</h2>
+		<ul class="inline-list styled-links">
+			<li><a href="http://www.linkedin.com/in/sarabine" title="LinkedIn" target="_blank">LinkedIn</a></li>
+			<li><a href="https://plus.google.com/102740217982737492275/" title="Google+" target="_blank">Google+</a></li>
+			<li><a href="http://www.facebook.com/sarabine" title="Facebook" target="_blank">Facebook</a></li>
+			<li><a class="email" title="Email me"></a></li>
+		</ul>
+	</div>
+</div>
+
+<?php include('footer.php'); ?>
