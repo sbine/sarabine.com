4cba5a4a147f8999310e6681f4eb5c26708455f0
Sara Bine <sara@sarabine.com>
2015-05-20 22:36:32 -0600

    Move JS to external file

--- index.php
+++ index.php
@@ -357,24 +357,6 @@
 	<footer>
 		<p>&copy; <?php echo date('Y'); ?> Sara Bine</p>
 	</footer>
-	<script>
-		var email = 'sara' + '' + '@' + 'sarabine' + '.' + 'com';
-		var emailElements = document.getElementsByClassName('email');
-		var item;
-		for (var i = 0; i < emailElements.length; i++) {
-			item = emailElements[i];
-			if (item.innerHTML.length === 0) {
-				item.innerHTML = item.innerHTML + email;
-			}
-			item.href = 'mailto:' + email;
-		}
-
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-16515493-1', 'sarabine.com');
-		ga('send', 'pageview');
-	</script>
+	<script src="assets/js/app.min.js"></script>
 </body>
 </html>
