88915c48ef2317cda3b8b0bc0a8cf5a2a98057ba
Sara Bine <sara@sarabine.com>
2019-05-24 16:12:11 -0700

    Remove Google Analytics

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,10 +9,3 @@ for (var i = 0; i < emailElements.length; i++) {
     item.href = 'mailto:' + email;
 }
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-16515493-1', 'auto');
-ga('send', 'pageview');
