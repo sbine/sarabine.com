47bae97a42ff80d8caf94773c2a287567a0e0518
Sara Bine <sara@sarabine.com>
2019-05-25 11:23:09 -0700

    Replace social icons with SVG versions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { colors } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   corePlugins: {
     preflight: false,
@@ -10,15 +12,23 @@ module.exports = {
     },
     extend: {
       colors: {
+        icon: {
+          email: '#E2A832',
+          github: '#181717',
+          linkedin: '#0077B5',
+          twitter: '#1DA1F2',
+        },
         white: '#FFF',
         black: '#000',
         gray: {
+          ...colors.gray,
           '100': '#FAFAFA',
           '200': '#F5F5F5',
           '300': '#E8E8E8',
           '400': '#DDDDDD',
         },
         red: {
+          ...colors.red,
           '600': '#DE0045',
           '700': '#CC0033',
           '800': '#770025',
@@ -28,7 +38,7 @@ module.exports = {
         '3xl': '2rem',
       },
       opacity: {
-        '80': '0.8',
+        '85': '0.85',
       },
       width: {
         '128': '32rem',
