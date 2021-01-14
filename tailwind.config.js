029048e040addae83c3a475ec5ef76bbceffbd33
Sara Bine <sara@sarabine.com>
2021-01-13 17:33:22 -0700

    Lighthouse optimizations, Update to Tailwind 2/Mix 6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const { colors } = require('tailwindcss/defaultTheme')
-
 module.exports = {
   purge: [
     './index.html',
@@ -12,6 +10,8 @@ module.exports = {
       heading: ['BebasNeue', 'serif'],
       body: [
         'SourceSansPro',
+        'ui-sans-serif',
+        'system-ui',
         '-apple-system',
         'BlinkMacSystemFont',
         '"Segoe UI"',
@@ -31,21 +31,28 @@ module.exports = {
           twitter: '#1DA1F2',
         },
         gray: {
-          ...colors.gray,
           '100': '#FAFAFA',
           '200': '#F5F5F5',
           '300': '#E8E8E8',
           '400': '#DDDDDD',
         },
         red: {
-          ...colors.red,
           '600': '#DE0045',
           '700': '#CC0033',
           '800': '#770025',
         }
       },
       fontSize: {
+        xs: '0.75rem',
+        sm: '0.875rem',
+        base: '1rem',
+        lg: '1.125rem',
         'xl': '1.4rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
       },
       letterSpacing: {
         wide: '0.02em',
